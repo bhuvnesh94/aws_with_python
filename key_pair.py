1d5d2b8a28f85428afe7fde0a53d07197292d15a
root <root@station46.example.com>
2019-06-19 12:55:25 +0530

    creating key-pair for instances

---
+++ key_pair.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import boto3
+ec2 = boto3.resource('ec2')
+
+# create a file to store the key locally
+outfile = open('ec2-keypair1.pem','w')
+
+# call the boto ec2 function to create a key pair
+key_pair = ec2.create_key_pair(KeyName='ec2-keypair1')
+
+# capture the key and store it in a file
+KeyPairOut = str(key_pair.key_material)
+print(KeyPairOut)
+outfile.write(KeyPairOut)
